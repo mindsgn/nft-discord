eb6184b8ec49e9a8c48dfca98a00e6a01f275c2a
mindsgn <48827267+mindsgn@users.noreply.github.com>
2022-07-22 16:21:59 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-from http import client
-import discord
+
+import logging
+import threading
 import os
+import asyncio
+from xml.etree.ElementTree import tostring
+import discord
+from dotenv import load_dotenv
+import socketio
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
+sio = socketio.AsyncClient()
 
 @client.event
 async def on_ready():
-    print ('ready {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('hello')
+    if message.content.startWith == '/help':
+        await message.channel.send()
+
+    if message.content.startWith == '/list-contracts':
+        await message.channel.send()
+
+@sio.event
+async def connect():
+    print('connection established')
+
+@sio.event
+async def my_message(data):
+    print('message received with ', data)
+    await sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+async def disconnect():
+    print('disconnected from server')
+
+async def connect():
+    await sio.connect('ws://api.diadata.org/ws/nft')
+    await sio.wait()
+
+def run_websocket():
+    pass
+
+def run_discord():
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    # b = threading.main_thread(target=run_discord)
+    # b.start()
+    a = threading.Thread(target=asyncio.run(connect()))
+    a.start()
     
-client.run(os.getenv('TOKEN'))
